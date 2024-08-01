40f291ed6096ee044230b41d163ec33a207b6677
takuminn8 <156915107+takuminn8@users.noreply.github.com>
2024-08-01 13:44:14 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+// 現在の日付を取得
+const today=new Date();
+
+const year=today.getFullYear();
+const month=today.getMonth() + 1;
+const date=today.getDate();
+
+// 年月日の文字列で出力
+console.log(year +'年'+month+'月'+date+'日');
